cb664646a768449ece109118a17019f26b2a5956
Ayesha <bibiayesha8024@gmail.com>
2023-06-21 13:41:27 +0530

    new file added

---
+++ learnjs/1. Introduction/script.js
@@ -0,0 +1,18 @@
+let x = 15;
+let y = 25;
+console.log("x and y is", x+y);
+
+//update the x
+x = 50;
+console.log("x and y is", x+y);
+
+let age = 24;
+let name = "Ayesha";
+console.log('Age =', age);
+console.log('Name is', name);
+
+//js is case sensitive
+let Age = 29;
+let Name = "Bi bi"
+console.log('Age =', Age);
+console.log('Name is', Name);
---
+++ learnjs/2. Data types/script.js
@@ -0,0 +1,15 @@
+//number type 
+let a = 2;
+console.log('a =', a);
+console.log('the type of variable a is', typeof(a));
+
+//string type
+let b = 'ayesha';
+console.log('b =', b);
+console.log('the type of variable b is', typeof(b));
+
+//boolean type
+let c = true;
+c = false;
+console.log('c =', c);
+console.log('the type of variable c is', typeof(c));
